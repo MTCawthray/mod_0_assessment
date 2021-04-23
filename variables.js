28b64df3cae819f8fa34a88b799b44a6191f438c
Mark Cawthray <dr.cawthray@gmail.com>
2021-04-23 13:04:13 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = "Nicholas Cage";
+var isActive = true;
+famousPerson = "Christopher Walken";
+var num3 = 11;
+var num9 = 42;
+var sum = (num9+num3);
+var animals = ["eagle", "shark", "duck"];
+animals.shift();
+console.log(famousPerson.length);
