ccd086c214830dd09224aeadfbf77c8911d01825
Mark Cawthray <dr.cawthray@gmail.com>
2021-04-23 13:15:05 -0600

    Add object to modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var denverInternationalAirport = {
+  location: "East Denver",
+  yearBuilt: "1995",
+  sizeInSquareMiles: 53,
+  flightsPerDay: 1700,
+  underConstruction: true,
+  internationalAirport: true,
+  featuresKnownFor: ["white roof", "scary mural", "underground train"],
+  carriers: ["United", "Frontier", "Southwest"]
+}
