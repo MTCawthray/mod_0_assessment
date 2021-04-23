e10780f9718967625699cc43001dad016c979c56
Mark Cawthray <dr.cawthray@gmail.com>
2021-04-23 13:21:08 -0600

    Add object to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var subaruOutback = {
+  color: "green",
+  year: "2010",
+  mileage: 170000,
+  NumnberOfSeats: 5,
+  hasAllWheelDrive: true,
+  hasSunRoof: false,
+  maintenanceDone: ["new tires", "replaced break pads", "changed oil"],
+  features: ["heated seats", "cd player", "leather seats"]
+}
